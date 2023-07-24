8be41f90915ca826d660386380da45a41f6728ab
2448322431@qq.com <mimajiushiwo>
2023-07-24 17:34:33 +0800

    测试

--- src/views/test/test1.vue
+++ src/views/test/test1.vue
@@ -8,6 +8,6 @@
     const time = parseInt(Date.now()/1000)
     const sign = md5(loginName + time + 'ES_HR')
     const click = ()=>{
-        window.open('http://xx.xxxx.com/Account/Sso.aspx?loginName='+ loginName +'&time='+ time +'&sign=' + sign,'_blank')
+        window.open('http://xxx.xxxx.com/Account/Sso.aspx?loginName='+ loginName +'&time='+ time +'&sign=' + sign,'_blank')
     }
 </script>
